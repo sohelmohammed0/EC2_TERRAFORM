f7fb0468be084f0a91e5cd5b0519bd650280c216
sohelmohammed0 <sohel88004329@gmail.com>
2025-01-20 16:48:00 +0530

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,83 @@
+provider "aws" {
+  region = var.region
+}
+
+# Data source for default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Data source for default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "default-for-az"
+    values = ["true"]
+  }
+}
+
+# Security Group
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security group for ${var.project_name} EC2 instance"
+  vpc_id      = data.aws_vpc.default.id
+
+  # SSH access
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.allowed_ssh_ips
+  }
+
+  # HTTP access
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rules
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-sg"
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  subnet_id     = tolist(data.aws_subnets.default.ids)[0]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
+
+  root_block_device {
+    volume_size           = var.volume_size
+    volume_type           = var.volume_type
+    encrypted             = var.encrypted_volume
+    delete_on_termination = true
+  }
+
+  tags = {
+    Name        = "${var.project_name}-instance"
+    Environment = var.environment
+    Owner       = var.owner
+  }
+}
